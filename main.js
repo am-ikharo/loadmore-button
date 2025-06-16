a9999cf8969480ca3e93722d4d64689fef4d35e6
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-16 23:55:26 +0100

    create the fetch api method

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 const productContainer = document.querySelector('product-container');
 
+
+async function fetchListOfProduct() {
+    try {
+        const response = await fetch('https://dummyjson.com/products?limit=10&skip=10&select=title,pri');
+    } catch (e) {
+        console.log(e);
+        
+    }
+}
+
+
+
+fetchListOfProduct();
