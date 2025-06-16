de3168c2a149403ba32174d7f13211324c740fc1
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-16 23:59:08 +0100

    log the result of the await function to see wat w are getting

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const productContainer = document.querySelector('product-container');
 
 async function fetchListOfProduct() {
     try {
-        const response = await fetch('https://dummyjson.com/products?limit=10&skip=10&select=title,pri');
+        const response = await fetch('https://dummyjson.com/products', {method: 'GET'});
+        const result = await response.json();
+        console.log(result);
+        
     } catch (e) {
         console.log(e);
         
