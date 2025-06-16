95cd49066279a4f34fd00b9ba564974730aa169d
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-17 00:25:33 +0100

    created the variable ror the element to be rendered in the html

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-const productContainer = document.querySelector('product-container');
-
+const productContainer = document.querySelector("product-container");
 
 async function fetchListOfProduct() {
-    try {
-        const response = await fetch('https://dummyjson.com/products', {method: 'GET'});
-        const result = await response.json();
-        console.log(result);
+  try {
+    const response = await fetch("https://dummyjson.com/products", {
+      method: "GET",
+    });
+    const result = await response.json();
+    if (result && result.products) displayProducts(result.products);
+  } catch (e) {
+    console.log(e);
+  };
+};
+
+function displayProducts(productList){
+
+    productList.forEach(productItem => {
+        const productItemWrapper = document.createElement('div');
+        const productTitle = document.createElement('p');
+        const productThumnail = document.createElement('img');
+        const productDescription = document.createElement('p');
+        const productPrice = document.createElement('p')
+
         
-    } catch (e) {
-        console.log(e);
-        
-    }
+    });
 }
-
-
 
 fetchListOfProduct();
